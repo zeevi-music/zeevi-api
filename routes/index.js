5a6ae3c629619be2a97445a5fd32cfdceaa66b88
Vicente Guerra Hernández <vicenteguerrahdez@gmail.com>
2015-03-13 16:40:10 -0600

    fix links (routes)

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+	router 	= express.Router(),
+	user_controller = require('./user.controller')(router)
+
+/* GET home page. */
+
+router.get('/', function(req, res) {
+  res.render('index')
+});
+
+router.get('/login', function(req, res) {
+  res.render('login');
+});
+
+router.get('/signup', function(req, res) {
+  res.render('signup');
+});
+
+module.exports = router;
