2bd7c1624eec7b6f68b1616b2dc76c7fdf052ff1
joseluisrosasbaza <Bazno379!>
2015-03-13 17:26:04 -0600

    Frontview CSS issues solved

--- public/js/div_manager.js
+++ public/js/div_manager.js
@@ -1,14 +1,18 @@
 function load_profile(){
   //alert("Reaching the load_profile function.");
-  document.getElementById("container").innerHTML='<object type="text/html" data="profile.html" style="width: 100%; height: 100%; padding: 0;"></object>';
+  document.getElementById("container").innerHTML='<object type="text/html" data="/htmls/profile.html" style="width: 100%; height: 100%; padding: 0;"></object>';
 }
 
 function load_sol(){
   //alert("Reaching the load_profile function.");
-  document.getElementById("container").innerHTML='<object type="text/html" data="solicitud.html" style="width: 100%; height: 100%; padding: 0;"></object>';
+  document.getElementById("container").innerHTML='<object type="text/html" data="/htmls/solicitud.html" style="width: 100%; height: 100%; padding: 0;"></object>';
 }
 
 function load_news(){
   //alert("Reaching the load_profile function.");
-  document.getElementById("container").innerHTML='<object type="text/html" data="news.html" style="width: 100%; height: 100%; padding: 0;"></object>';
+  document.getElementById("container").innerHTML='<object type="text/html" data="/htmls/news.html" style="width: 100%; height: 100%; padding: 0;"></object>';
+}
+
+function message(){
+  alert("This is a message.");
 }
