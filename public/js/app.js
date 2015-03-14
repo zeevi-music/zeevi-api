35186b1a8c8ae0974f51b95d9780c2fbd7e623e5
Alejandro Cuenca Estrada <alexcuenca4@gmail.com>
2015-03-13 20:17:47 -0600

    commit before routing with angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,35 @@
 var app = angular.module('app',[]);
 
-/* Controllers functions */
+/*app configurations */
 
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+
+      when('/user/home/profile', {
+        templateUrl: '/htmls/profile',
+        controller: 'userCtrl'
+      })
+
+  }]);
+
+/* Controllers functions */
 var userController = function(scope,window,users) {
 	scope.user = {};
 	scope.related = [];
 
-	/*scope.user.username = window.location.href//.substring(32)
-	console.log(scope.user.username);*/
+	scope.user.username = window.location.href.substring(32)
 	
 	users.get_all_user_data(scope.user.username)
 		.then(function (data){
-			console.log(data)
 			scope.user = data.data;
 			console.log(scope.user);
 
 			users.get_all_users_by_genre(scope.user.genre)
-			.then(function (data){
-				console.log(data)
+			.then(function (data){				
 				scope.related = data.data.data;
-				//console.log(scope.user);
 			});
-
-		});
-
-	
+		});	
 };
 
 /* Services */
@@ -34,11 +39,10 @@ var userService = function(http) {
 	result.get_all_user_data = function (username){
 		return http({
 			method: 'GET',
-			url: '/api/user/'+'Slayer'
+			url: '/api/user/'+username
 		});
 	}
-	result.get_all_users_by_genre = function (genre){
-		console.log(genre)
+	result.get_all_users_by_genre = function (genre){		
 		return http({
 			method: 'GET',
 			url: '/api/users/'+genre
