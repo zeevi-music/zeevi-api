98d4ab6f640a8ba4c0255a18a2dabcf52ac8dd5b
Vicente Guerra Hernández <vicenteguerrahdez@gmail.com>
2015-03-13 20:19:04 -0600

    Login / cifrado password

--- routes/api.controller.js
+++ routes/api.controller.js
@@ -4,20 +4,52 @@ module.exports = function(router){
 
 	router.post('/api/user/new', function(req,res){
 		var username = req.body.username;
+		var password = req.body.password;
+		var passCifrado= cifrar(username, password)
 		var newUser = new user({
 			username: username,
 			email: req.body.email,
-			password: req.body.password,
+			password: passCifrado,
 			profile: req.body.tipo
 		});
 		newUser.save();
 		res.redirect('/user/home/'+username);
 	});
 
-
-	router.post('/api/user/new', function(req,res){
+	router.post('/api/user/login', function(req,res){
 		var username = req.body.username;
+		var pass = req.body.password;
+		var passCifrado = cifrar(username,pass);
+		user.findOne({username: username}, function(err,user) {
+			console.log(err);
+
+			if(user === null){
+				console.log("No encontro");
+				res.redirect('/login');
+			}
+			else{
+				var pass1 = user.password;
+				if(pass1 === passCifrado){
+					console.log('INICIO ')
+					res.redirect('/user/home/'+username);
+				}
+				else{
+					console.log("No encontro");
+					res.redirect('/login');
+				}
+			}
+
+		});
+
 	});
 
+
 };
-//res.status(200).json({});
+
+
+function cifrar(user, pass) {
+   var crypto = require('crypto')
+   // usamos el metodo CreateHmac y le pasamos el parametro user y actualizamos el hash con la password
+   var hmac = crypto.createHmac('sha1', user).update(pass).digest('hex')
+   return hmac
+}
