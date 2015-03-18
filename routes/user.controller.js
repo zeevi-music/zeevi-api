c3de53cd47d87921b488cdfe531a59fa680539e3
Ricardo <ricktter@gmail.com>
2015-03-18 17:27:11 -0600

    frontend and backend joined

--- routes/user.controller.js
+++ routes/user.controller.js
@@ -1,14 +1,13 @@
 module.exports = function(router){
 
 	router.get('/user/home/:username', function(req,res){
-		/*
+		
 		if( req.param('username') == req.session.userName ){
 			res.render('home',{username: req.param('username')});
 		}
 		else{
 			res.redirect('/login');
 		}
-		*/
 		res.render('home',{username: req.param('username')});
 	});
 };
