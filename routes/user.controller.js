e68c9448e4a0c172235c7e3b04ad6872436607b8
Vicente Guerra Hernández <vicenteguerrahdez@gmail.com>
2015-03-17 18:43:36 -0600

    Solicitudes View

--- routes/user.controller.js
+++ routes/user.controller.js
@@ -3,4 +3,8 @@ module.exports = function(router){
 	router.get('/user/home/:username', function(req,res){
 		res.render('home',{username: req.param('username')});
 	});
+
+	router.get('/user/home/:username/solicitudes', function(req, res) {
+	  res.render('solicitud');
+	});
 };
