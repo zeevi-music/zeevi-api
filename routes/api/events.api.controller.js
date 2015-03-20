0ffd80329176491420ce5352a92cfcc5f54c091d
Ricardo <ricktter@gmail.com>
2015-03-20 15:25:52 -0600

    Labels in spanish, code in english

--- routes/api/events.api.controller.js
+++ routes/api/events.api.controller.js
@@ -13,27 +13,25 @@ module.exports = function (router) {
       band_name:  band_name,
       venue_name: venue_name
     })
-    .exec(function(err,doc){
-      if(err) 
-        res.send(err);
-      else
-        if(doc)
-        {
-          if( !doc.active && !doc.rejected )
+      .exec(function (err, doc) {
+        if (err) {
+          res.send(err);
+        } else if (doc) {
+          if (!doc.active && !doc.rejected) {
             res.send("1");
-          if(doc.rejected)
+          }
+          if (doc.rejected) {
             res.send("2");
-          if(doc.active)
+          }
+          if (doc.active) {
             res.send("3");
-        }
+          }
+        } else {
+          var request_to = req.body.request_to,
+            hour = req.body.hour,
+            date = req.body.date;
 
-        else{
-          var request_to  = req.body.request_to;
-              hour        = req.body.hour,
-              date        = req.body.date;
-
-          console.log(req.body.hour)
-
+          console.log(req.body.hour);
           var newEvent = new Event({
             band_name:  band_name,
             venue_name: venue_name,
@@ -43,62 +41,62 @@ module.exports = function (router) {
           newEvent.save(function (err, data) {
             if (err) {
               res.send(err);
-            }
-            else
+            } else {
               res.json(data);
+            }
           });
         }
-    });
-
+      });
   });
 
   // Event Confirmation
-  router.put('/api/events/:type/:username', function(req,res){
-    var username =  req.params.username.replace('_',' ');
-    var userType =  req.params.type;
+  router.put('/api/events/:type/:username', function (req, res) {
+    var username = req.params.username.replace('_', ' ');
+    var userType = req.params.type;
     var query;
-    if(userType == 'band'){
+    if (userType === 'band') {
       query = {band_name: username};
-    }else{
+    } else {
       query = {venue_name: username};
     }
-    
-    Event.findOne(query, function(err, doc){
+    Event.findOne(query, function (err, doc) {
 
-        console.log(req.body);
+      console.log(req.body);
 
-        if (err) return res.send(500, { error: err });
-        doc.active = req.body.active,
-        doc.rejected = req.body.rejected
+      if (err) {
+        res.send(500, { error: err });
+      }
+      doc.active = req.body.active;
+      doc.rejected = req.body.rejected;
 
-        if(req.body.active = true)
-        {
-          doc.date = req.body.date,
-          doc.hour = req.body.hour
+      if (req.body.active === true) {
+        doc.date = req.body.date;
+        doc.hour = req.body.hour;
+      }
+
+      doc.save(function (err, doc) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.status(200).json(doc);
         }
-
-        doc.save(function(err, doc){
-          if(err)
-            res.send(err);
-          else
-            res.status(200).json(doc)
-        });
+      });
     });
   });
 
   // GET events
-  router.get('/api/events/:type/:username', function(req, res){
-    var username = req.params.username.replace('_',' ');
-    var userType =req.params.type;
-    console.log(" GET events "+userType);
+  router.get('/api/events/:type/:username', function (req, res) {
+    var username = req.params.username.replace('_', ' ');
+    var userType = req.params.type;
+    console.log(" GET events " + userType);
     var query;
-    if(userType == 'band'){
-      query = {band_name: username, request_to: username, active:false, rejected:false};
-    }else{
-      query = {venue_name: username, request_to: username, active:false, rejected:false};
+    if (userType === 'band') {
+      query = {band_name: username, request_to: username, active: false, rejected: false};
+    } else {
+      query = {venue_name: username, request_to: username, active: false, rejected: false};
     }
-    Event.find(query,function(err,data){
-      if(err){
+    Event.find(query, function (err, data) {
+      if (err) {
         console.log("Error");
       }
       console.log("Consultado");
@@ -107,17 +105,15 @@ module.exports = function (router) {
   });
 
   //GET active events
-  router.get('/api/events/active',function(req,res){
+  router.get('/api/events/active', function (req, res) {
     Event
-    .find({active:true})
-    .exec(function(err,doc){
-      if(err)
-        res.send(err);
-      else
-        res.json(doc);
-    });
+      .find({active: true})
+      .exec(function (err, doc) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.json(doc);
+        }
+      });
   });
-
 };
-
-
