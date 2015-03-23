eccfe2187551973e5223a8bc60be4cc5f76677c7
joseluisrosasbaza <Bazno379!>
2015-03-22 20:09:51 -0600

    Profile menu pimped

--- public/js/vendor/event-handler.js
+++ public/js/vendor/event-handler.js
@@ -20,3 +20,22 @@ function disscardEvent(){
 function rating(rating){
   alert("Calificaste al usuario con " + rating + " estrellas.");
 }
+
+function showMenu(){
+  var shadow = document.getElementById("dark");
+  var menu = document.getElementById("menu");
+  
+  $(shadow).fadeIn("fast");
+  
+  menu.style.display = "";
+  $(menu).animate({width: "260px"}, 300);
+}
+
+function hideMenu(){
+  var shadow = document.getElementById("dark");
+  var menu = document.getElementById("menu");
+  
+  $(menu).animate({width: "280px"});
+  menu.style.display = "none";
+  $(shadow).fadeOut("fast");
+}
